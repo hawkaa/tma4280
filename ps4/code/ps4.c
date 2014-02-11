2239a49d23417949f301af54ff80f49c3d5f5508
Håkon Åmdal <hakon@aamdal.com>
2014-02-11 17:57:28 +0100

    Legger alt i én fil. Oppgave 1 er løst

--- ps4/code/ps4.c
+++ ps4/code/ps4.c
@@ -1,17 +1,89 @@
+/*
+ * Problem set 4
+ * TMA4280
+ * Håkon Åmdal
+ */
 
+/* system headers */
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-#include "ps4_util.h"
 
-static int n = 5;
+/*
+ * Constants for determining the output, as specified in the task.
+ */
+static const int K_MIN = 3;
+static const int K_MAX = 14;
 
-int main(int argc, char** argv)
+
+/*
+ * Fills a vector of length n with the inverse squared of the index (as 
+ * specified in the task).
+ */
+static void
+fill_invsquare_vector(int n, double *v)
 {
+	int i, k;
+	for(i = 0; i < n; ++i) {
+		k = i + 1; 
+		v[i] = 1.0 / (k * k);
+	}
+}
+
+/*
+ * Calculates the sum of an array of doubles with the length n.
+ */
+static double
+sum_vector(int n, double *v)
+{
+	double sum;
+	int i;
+	sum = 0.0;
+	for(i = 0; i < n; ++i) 
+		sum += v[i];	
+	return sum;
+}
+
+/*
+ * Power function for integers.
+ */
+static int
+intpow(int b, int e)
+{
+	int r;
+	r = b;
+	while(e > 1){
+		r = r * b;
+		--e;
+	}
+	return r;
+}
+
+/*
+ * Program entry function.
+ */
+int
+main(int argc, char** argv)
+{
+	double pi_reference, sum_reference, sum;
+	int n, k;
 	
-	double *v = (double*)malloc(sizeof(double)*n);
+	/* reference values */
+	pi_reference = 4.0 * atan(1.0);
+	sum_reference = pi_reference * pi_reference / 6.0;
 	
-	fill_vector(n, v);
-	printf("hehe%f\n", v[2]);
-	return 0;
+	/* allocating and filling vector with values */
+	n = intpow(2, K_MAX);
+	double *v = malloc(sizeof (double) * n);
+	fill_invsquare_vector(n, v);
+
+	/* summing and printing results */
+	for(k = K_MIN; k <= K_MAX; ++k) {
+		sum = sum_vector(intpow(2,k), v);
+		printf("%i:\t %e\n", k, sum-sum_reference);
+	}
+
+	/* preferred over return statements to end application */
+	exit(0);
 }
--- ps4/code/ps4_util.h
+++
@@ -1,9 +0,0 @@
-/*
- * Utility module
- */
-#ifndef PS4_UTIL_H_
-#define PS4_UTIL_H_
-
-extern void fill_vector(int /*vector_size*/, double* /*vector*/);
-
-#endif
